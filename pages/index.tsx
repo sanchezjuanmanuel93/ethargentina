51973d7c6f3244d043b264315aa2ad6dcea04e63
Juan Manuel Sanchez <juanmanuelsanchez@Juans-MacBook-Pro.local>
2023-01-21 20:51:22 -0300

    Impl i18next

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,18 @@
 import Head from 'next/head'
-import { Inter } from '@next/font/google'
+import { Header } from "../components/header"
+import { useTranslation, Trans } from 'next-i18next'
+import type { GetStaticProps, InferGetStaticPropsType } from 'next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
-const inter = Inter({ subsets: ['latin'] })
+type Props = {
+  // Add custom props here
+}
 
-export default function Home() {
+const Home = (
+  _props: InferGetStaticPropsType<typeof getStaticProps>
+) => {
+  const { t } = useTranslation('home');
+
   return (
     <>
       <Head>
@@ -13,17 +22,12 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <header>
-        <nav>
-          <li><a href="https://t.me/+2nijw5YHq_hjODkx" target="_blank" rel="noopener noreferrer">Telegram</a></li>
-          <li><a href="https://twitter.com/etherargentina?s=11&t=j_w3njfNO2boGMHoQgfS3Q" target="_blank" rel="noopener noreferrer">Twitter</a></li>
-        </nav>
-      </header>
+      <Header />
 
       <section className="home">
-        <h1>ETHEREUM ARGENTINA</h1>
-        <h3>El evento de Ethereum más grande de Argentina 🚀</h3>
-        <a href="https://t.me/+2nijw5YHq_hjODkx" className="btn">¡Unite a Telegram!</a>
+        <h1>{t('title')}</h1>
+        <h3>{t('subTitle')}</h3>
+        <a href="https://t.me/+2nijw5YHq_hjODkx" className="btn">{t("joinTelegram")}</a>
       </section>
 
       <footer>
@@ -121,81 +125,6 @@ export default function Home() {
           flex-wrap: wrap;
           justify-content: center;
         }
-
-
-        /*-------------
-          Header
-        -------------*/
-
-        header{
-          position: absolute;
-          top: 0;
-          left: 0;
-          z-index: 10;
-          width: 100%;
-          display: flex;
-          justify-content: space-between;
-          align-items: center;
-          color: #fff;
-          padding: 35px 100px 0;
-          background-color: #5989BD;
-        }
-
-        header nav{
-          display: flex;
-        }
-
-        header nav li{
-          margin: 0 15px;
-          display: inline-block;
-          position: relative;
-          padding-bottom: 3px;
-        }
-
-        header nav li:first-child{
-          margin-left: 0;	
-        }
-
-        header nav li:last-child{
-          margin-right: 0;	
-        }
-
-        header nav li::after {
-          content: '';
-          position: absolute;
-          width: 100%;
-          transform: scaleX(0);
-          height: 1px;
-          bottom: 0;
-          left: 0;
-          background-color: #fff;
-          transform-origin: bottom right;
-          transition: transform 0.25s ease-out;
-        }
-        
-        header nav li:hover::after {
-          transform: scaleX(1);
-          transform-origin: bottom left;
-        }
-
-        @media (max-width: 1000px){
-          header{
-            padding: 20px 50px;
-          }
-        }
-
-
-        @media (max-width: 700px){
-          header{
-            flex-direction: column;		
-          }
-
-          header h2{
-            margin-bottom: 15px;
-          }
-        }
-
-
 
         /*----------------
           Home Section
@@ -330,3 +259,14 @@ export default function Home() {
     </>
   )
 }
+
+export const getStaticProps: GetStaticProps<Props> = async ({
+  locale,
+}) => ({
+  props: {
+    ...(await serverSideTranslations(locale ?? 'en', ['home'])),
+  },
+})
+
+
+export default Home;
